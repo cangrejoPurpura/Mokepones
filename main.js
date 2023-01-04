e19c18a741f6cf414a33d57a9b01fb65f7924b73
Sebastián Salvatierra <focusyn.2506@gmail.com>
2023-01-04 19:00:33 -0300

    2da clase

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-const boton = document.getElementById('botoncito')
+window.addEventListener('load', iniciarJuego)
 
-addEventListener(boton, alert('hola'));
+function iniciarJuego(){
+    const botonMascota = document.getElementById('boton-mascota')
+    botonMascota.addEventListener('click', seleccionarMascotaJugador)
+
+}
+function seleccionarMascotaJugador(){
+
+    let inputHipodoge = document.getElementById('hipodoge');
+    let inputCapipepo = document.getElementById('capipepo');
+    let inputRatigueya = document.getElementById('ratigueya');
+
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+
+    if(inputHipodoge.checked){
+        spanMascotaJugador.innerHTML = 'hipodoge'
+    } else if (inputCapipepo.checked){
+        spanMascotaJugador.innerHTML = 'capipepo'
+    } else if(inputRatigueya.checked){
+        spanMascotaJugador.innerHTML = 'ratigueya'
+    } else {
+        alert('por favor, selecciona una mascota')
+    }
+    
+}
+const botonFuego = document.getElementById('boton-fuego')
+const botonAgua = document.getElementById('boton-agua')
+const botonTierra = document.getElementById('boton-tierra')
+
